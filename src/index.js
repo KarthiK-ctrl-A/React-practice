e7ff5d05b2ddc75c602704abe3f39bd5160e95ef
Karthik-ctrl-A <saikarthik4me@gmail.com>
2021-02-07 08:17:37 +0530

    components commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-setInterval(function(){
+function Cartoon(props){
+  return <h1> Hello , {props.name} on {props.show} </h1>
+ }
 
-  ReactDom.render(
-    <h2>Time Now: {new Date().toLocaleTimeString()}</h2> ,
-    document.getElementById('root')
-  );
-},1000);
+ function Show(){
+   return <div>
+     <Cartoon name='Tom' show='Tom and jerry'/>
+     <Cartoon name='Jack' show='Jack and Jill' />
+   </div>
+ }
 
+ class Car extends React.Component{
+  render(){
+    return <h1>Car componets value {this.props.name} </h1>
+  }
+}
+
+ ReactDom.render(
+  <Show />,
+  document.getElementById('root')
+);
